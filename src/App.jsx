4b6016bc4d028d04d58872c0c23fb0886d1b6f8d
Irshad <125551821+irshad0075@users.noreply.github.com>
2023-11-04 07:03:20 -0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import react from "react";
+import Home from "./Component/Home/Home";
+import Nav from "./Component/Navs/navs";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Nav />
+      <Home />
+    </>
+  );
+}
+
+export default App;
